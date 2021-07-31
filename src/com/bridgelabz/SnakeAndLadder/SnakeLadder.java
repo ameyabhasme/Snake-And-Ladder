3668e14ef2fc2eb282c7d9017028f0946330ccaa
ameyabhasme <ameyabhasme5@gmail.com>
2021-07-31 15:00:19 +0530

    Added: Random function to roll dice between 1 to 6

--- src/com/bridgelabz/SnakeAndLadder/SnakeLadder.java
+++ src/com/bridgelabz/SnakeAndLadder/SnakeLadder.java
@@ -6,6 +6,7 @@ public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake And Ladder game");
 		System.out.println("\nStarting the game with Game Point= " + START);
-		
+		int dice = (int) (Math.random() * 6) + 1;
+		System.out.println("Dice roll: " + dice);
 	}
 }
