1831e09eaa0efeb5a53fd312d2f6e3fe9360050d
ameyabhasme <ameyabhasme5@gmail.com>
2021-07-31 14:54:21 +0530

    Added: Start position in game

--- src/com/bridgelabz/SnakeAndLadder/SnakeLadder.java
+++ src/com/bridgelabz/SnakeAndLadder/SnakeLadder.java
@@ -1,8 +1,11 @@
 package com.bridgelabz.SnakeAndLadder;
 
 public class SnakeLadder {
+	final static int START = 0;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake And Ladder game");
+		System.out.println("\nStarting the game with Game Point= " + START);
+		
 	}
 }
