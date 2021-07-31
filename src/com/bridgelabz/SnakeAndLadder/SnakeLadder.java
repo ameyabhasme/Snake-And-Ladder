8926f0b566b0db65872128afd5f996c4847a63d0
ameyabhasme <ameyabhasme5@gmail.com>
2021-07-31 16:02:49 +0530

    Added: Win Position

--- src/com/bridgelabz/SnakeAndLadder/SnakeLadder.java
+++ src/com/bridgelabz/SnakeAndLadder/SnakeLadder.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.SnakeAndLadder;
 
 public class SnakeLadder {
-	final static int START = 0;
+	static final int START = 0;
+	static final int WIN_POSITION = 100;
 	static final int NO_PLAY = 0;
 	static final int LADDER = 1;
 	static final int SNAKE = 2;
@@ -9,28 +10,31 @@ public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake And Ladder game");
 		System.out.println("\nStarting the game with Game Point= " + START);
-		int dice = (int) (Math.random() * 6) + 1;
-		System.out.println("Dice roll: " + dice);
-		
+
 		int position = 0;
 		int newPosition = 0;
-		
-		int checkOption = (int) (Math.random() * 3);
-		
-		switch (checkOption) {
-		case NO_PLAY:
-			newPosition = 0;
-			break;
-		case LADDER:
-			newPosition = dice;
-			break;
-		case SNAKE:
-			newPosition = -dice;
+
+		while (position != WIN_POSITION) {
+
+			int dice = (int) (Math.random() * 6) + 1;
+			System.out.println("Dice roll: " + dice);
+			int checkOption = (int) (Math.random() * 3);
+
+			switch (checkOption) {
+			case NO_PLAY:
+				newPosition = 0;
+				break;
+			case LADDER:
+				newPosition = dice;
+				break;
+			case SNAKE:
+				newPosition = -dice;
+			}
+			position = position + newPosition;
+			if (position < 0) {
+				position = 0;
+			}
+			System.out.println("Position: " + position);
 		}
-		position = position + newPosition;
-		if (position < 0) {
-			position = 0;
-		}
-		System.out.println("Position: "  + position);
 	}
 }
